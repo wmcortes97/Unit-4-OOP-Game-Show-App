6bb769f00fa028e36f88e862e6803072773a1d73
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-20 21:02:19 -0700

    starting game using add event listener

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -1,6 +1,12 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-const game = new Game();
-game.startGame();
-console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
+let game;
+
+let startButton = document.getElementById("btn__reset");
+
+startButton.addEventListener("click", (game = new Game()).startGame);
+
+// const game = new Game();
+// game.startGame();
+// console.log(`Active Phrase - phrase: ${game.activePhrase.phrase}`);
