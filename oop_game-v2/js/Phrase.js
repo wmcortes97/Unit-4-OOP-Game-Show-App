6f03635b678639e8ee1360852bf2ec9f597cc402
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-20 20:00:28 -0700

    created addPhraseToDisplay Function

--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -5,4 +5,33 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+  addPhraseToDisplay() {
+    let splitPhrase = this.phrase.split("");
+    let html = "";
+
+    for (let i = 0; i < splitPhrase.length; i++) {
+      if (splitPhrase[i] !== " ") {
+        html += `<li class="hide letter ${splitPhrase[i]}">${splitPhrase[i]}</li>`;
+      } else {
+        html += `<li class="space">" "</li>`;
+      }
+    }
+
+    document.querySelector("#phrase ul").innerHTML = html;
+  }
 }
+
+// let example = "You are awesome";
+// let splitPhrase = example.split("");
+
+// let html = "";
+
+// for (let i = 0; i < splitPhrase.length; i++) {
+//   if (splitPhrase[i] !== " ") {
+//     html += `<li class="hide letter ${splitPhrase[i]}">${splitPhrase[i]}</li>`;
+//   } else {
+//     html += `<li class="space">" "</li>`;
+//   }
+// }
+
+// document.querySelector("#phrase ul").innerHTML = html;
--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -2,12 +2,5 @@
  * Project 4 - OOP Game App
  * app.js */
 
-const logPhrase = (phrase) => {
-  console.log(`Phrase - phrase: `, phrase.phrase);
-};
 const game = new Game();
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
-logPhrase(game.getRandomPhrase());
+game.getRandomPhrase().addPhraseToDisplay();
