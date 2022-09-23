804c01e0e4058841e2f98ee7180770362aeb047e
wmcortes97 <94777765+wmcortes97@users.noreply.github.com>
2022-09-23 01:03:09 -0700

    completed addEventListener to rest when starting  game again

--- oop_game-v2/js/app.js
+++ oop_game-v2/js/app.js
@@ -6,6 +6,22 @@ let game;
 let startButton = document.getElementById("btn__reset");
 
 startButton.addEventListener("click", () => {
+  let phrase = document.querySelectorAll("#phrase li");
+  for (let i = 0; i < phrase.length; i++) {
+    phrase[i].remove();
+  }
+
+  let keyButtons = document.querySelectorAll(".key");
+  for (let i = 0; i < keyButtons.length; i++) {
+    keyButtons[i].disabled = false;
+    keyButtons[i].className = "key";
+  }
+
+  let resetHearts = document.querySelectorAll("#scoreboard img");
+  for (let i = 0; i < resetHearts.length; i++) {
+    resetHearts[i].setAttribute("src", "images/liveHeart.png");
+  }
+
   (game = new Game()).startGame();
 });
 
